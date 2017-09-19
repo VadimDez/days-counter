7c5a148d824a01dec77080b3ef18efe3dc88af65
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-09-19 22:18:47 +0200

    Persist data. +1 available only after 24hours

--- Days/Data.swift
+++ Days/Data.swift
@@ -9,27 +9,39 @@
 import UIKit
 import os.log
 
-class Data: NSCoder {
+class Data: NSObject, NSCoding {
     var count: UInt!
+    var lastSuccessUpdate: Date!
     
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentsDirectory.appendingPathComponent("data")
     
     struct PropertyKey {
         static let count = "count";
+        static let lastSuccessUpdate = "updated";
     }
     
-    init?(count: UInt) {
+    init?(count: UInt = 0, lastSuccessUpdate: Date?) {
         self.count = count;
-    }
-    
-    required convenience init?(coder aDecoder: NSCoder) {
-        let count = UInt(aDecoder.decodeInteger(forKey: PropertyKey.count))
-        
-        self.init(count: count);
+        self.lastSuccessUpdate = lastSuccessUpdate;
     }
     
     func encode(with aCoder: NSCoder) {
         aCoder.encode(self.count, forKey: PropertyKey.count)
+        aCoder.encode(self.lastSuccessUpdate, forKey: PropertyKey.lastSuccessUpdate)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let count = aDecoder.decodeObject(forKey: PropertyKey.count) as! UInt
+        guard let lastSuccessUpdate = aDecoder.decodeObject(forKey: PropertyKey.lastSuccessUpdate) as? Date else {
+            return nil;
+        }
+        
+        self.init(count: count, lastSuccessUpdate: lastSuccessUpdate);
+    }
+    
+    func reset() -> Void {
+        self.count = 0;
+        self.lastSuccessUpdate = nil;
     }
 }
--- Days/ViewController.swift
+++ Days/ViewController.swift
@@ -20,8 +20,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.loadData();
-        
-        self.updateLabel();
+        self.update();
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,35 +30,39 @@ class ViewController: UIViewController {
 
     @IBAction func onSuccess(_ sender: Any) {
         self.data.count = self.data.count + 1;
+        self.data.lastSuccessUpdate = Date.init();
         self.saveData();
-        self.updateLabel();
+        self.update();
     }
 
     @IBAction func onReset(_ sender: Any) {
-        self.data.count = 0;
-//        self.saveData();
-        self.updateLabel();
+        self.data.reset();
+        self.saveData();
+        self.update();
     }
     
     func updateLabel() -> Void {
-        self.daysLabel.text = "\( self.data.count )";
+        self.daysLabel.text = "\( self.data.count! )";
+    }
+    
+    func updateButton() -> Void {
+        self.successButton.isEnabled = self.data.lastSuccessUpdate == nil || self.data.lastSuccessUpdate.addingTimeInterval(24 * 60 * 60) <= Date.init();
+    }
+    
+    func update() {
+        self.updateLabel();
+        self.updateButton();
     }
     
     private func saveData() {
-        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(self.data, toFile: Data.ArchiveURL.path)
-        
-        if isSuccessfulSave {
-            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
-        } else {
-            os_log("Failed to save meals...", log: OSLog.default, type: .error)
-        }
+        NSKeyedArchiver.archiveRootObject(self.data, toFile: Data.ArchiveURL.path)
     }
     
     private func loadData() {
         if let data = NSKeyedUnarchiver.unarchiveObject(withFile: Data.ArchiveURL.path) as? Data {
             self.data = data;
         } else {
-            self.data = Data(count: 0);
+            self.data = Data(count: 0, lastSuccessUpdate: nil);
         }
     }
 }
